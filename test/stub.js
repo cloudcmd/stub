f95689cf5817e69cacab372709cf22e8fb32f10d
coderaiser <coderaiser@cloudcmd.io>
2019-08-26 21:43:31 +0300

    feature(stub) add madrun

--- test/stub.js
+++ test/stub.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const test = require('tape');
+const test = require('supertape');
 const tryToCatch = require('try-to-catch');
 const {reRequire} = require('mock-require');
 const stub = require('..');
