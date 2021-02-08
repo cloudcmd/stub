4246c9d7d9d2b1d790d0b3fa88c540ba16ac0d44
coderaiser <coderaiser@cloudcmd.io>
2021-02-08 11:44:41 +0200

    chore(stub) lint

--- .madrun.mjs
+++ .madrun.mjs
@@ -1,11 +1,11 @@
-'use strict';
+import {run} from 'madrun';
 
-const {run} = require('madrun');
-
-module.exports = {
+export default {
     'test': () => 'tape \'test/*.js\'',
     'watch:test': () => 'nodemon -w lib -w test -x "npm test"',
     'lint': () => 'putout .',
+    'fresh:lint': () => run('lint', '--fresh'),
+    'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
     'coverage': () => 'nyc npm test',
     'report': () => 'nyc report --reporter=text-lcov | coveralls',
