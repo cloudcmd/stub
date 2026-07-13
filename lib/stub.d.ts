46e5d57e53ba608ee38876602bf27761d8ffb731
coderiaser <coderaiser@cloudcmd.io>
2026-07-13 11:49:41 +0300

    feature: @supertape/stub: improve types

--- lib/stub.d.ts
+++ lib/stub.d.ts
@@ -1,19 +1,32 @@
-export type Stub = Function & {
+export function stub<
+    Args extends any[] = any[],
+>(
+    fn?: (...args: Args) => unknown,
+): Stub<Args, void>;
+
+export type Stub<
+    Args extends unknown[] = unknown[],
+    Return = unknown,
+> = ((...args: Args) => Return) & {
     called: boolean;
     callId: number;
-    args: unknown[];
+
+    args: Args[];
     callCount: number;
-    value: unknown;
-    calledWith: (...args: unknown[]) => boolean;
-    calledBefore: (fn: Stub) => boolean;
-    calledAfter: (fn: Stub) => boolean;
-    withName: (name: string) => Stub;
-    throws: (error: Error) => Stub;
-    rejects: (error: Error) => Promise<Stub>;
-    resolves: (value: unknown) => Stub;
-    returns: (value: unknown) => Stub;
+    value: Return;
+
+    calledWith: (...args: Args) => boolean;
+
+    calledBefore: (fn: Stub<any, any>) => boolean;
+    calledAfter: (fn: Stub<any, any>) => boolean;
+
+    withName: (name: string) => Stub<Args, Return>;
+
+    returns: <T>(value: T) => Stub<Args, T>;
+    resolves: <T>(value: T) => Stub<Args, Promise<T>>;
+
+    throws: (error: Error) => Stub<Args, never>;
+    rejects: (error: Error) => Stub<Args, Promise<never>>;
 };
 
-export function stub(fn?: Function): Stub;
-export function isStub(fn: Stub | Function): Boolean;
-
+export function isStub(fn: unknown): fn is Stub;
--- test/errors.ts
+++ test/errors.ts
@@ -1,6 +1,6 @@
 import {stub, isStub} from '../lib/stub.js';
 
-// THROWS Type 'Stub' is not assignable to type 'string'.
+// THROWS Type 'Stub<any[], void>' is not assignable to type 'string'.
 const a: string = stub();
 
 const fn = stub();
@@ -18,7 +18,16 @@ fn.withName('hello');
 // THROWS Type 'boolean' is not assignable to type 'string'.
 const b: string = fn.calledWith();
 
-// THROWS Type 'Boolean' is not assignable to type 'number'.
+// THROWS Type 'boolean' is not assignable to type 'number'.
 const c: number = isStub(fn);
 
 fn(b, c);
+
+type Log = (message: string) => void;
+
+const log = stub<[string]>();
+
+const typedLog: Log = log;
+
+typedLog('hello');
+
